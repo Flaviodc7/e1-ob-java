920eea13473d78e56bb05df6334440200e11022b
Flavio Coscarella <flaviodc@live.com.ar>
2022-11-14 10:05:39 -0300

    Ejercicio del curso

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int numero = 429744;
+        long longInt = 4229467444489L;
+        double decimal = 346.24;
+        boolean cierto = false;
+        String saludo = "Hola Mundo";
+        System.out.println("Integer: " + numero);
+        System.out.println("Long Integer: " + longInt);
+        System.out.println("Decimal: " + decimal);
+        System.out.println("Boolean: " + cierto);
+        System.out.println("Saludo: " + saludo);
+    }
+}
